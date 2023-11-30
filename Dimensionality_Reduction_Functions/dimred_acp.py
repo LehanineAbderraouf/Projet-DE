1201653673bbf4b5d9ce52acd8829f732739e808
raouf <raouflehanine@gmail.com>
2023-11-30 12:12:22 +0100

    fix typo

---
+++ Dimensionality_Reduction_Functions/dimred_acp.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import prince
+
+def dimred_acp(x, n_components):
+  '''
+	Perform dimensionality reduction with the ACP function
+
+	Input:
+	-----
+	    x : NxM list (numpy array)
+	    n_components : number of dimensions to keep 
+	Output:
+	------
+	    red_mat : NxP list such that p<<m
+  '''
+  df = pd.DataFrame(x)
+  pca = prince.PCA(n_components=n_components)
+  pca = pca.fit(df)
+  return pca.transform(df)
